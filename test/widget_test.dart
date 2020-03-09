33ec3097e2b4b7ac249fb500f6e31c9e3d3679a6
Farbota <Steven.Farbota@coleparmer.com>
2020-03-08 17:27:25 -0700

    Remove unusued function & fix test logic.

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -15,7 +15,7 @@ void main() {
     // Build our app and trigger a frame.
     await tester.pumpWidget(MyApp());
 
-    // Verify that our counter starts at 0.
+    // Verify that our counter starts at 1.
     expect(find.text('1'), findsOneWidget);
     expect(find.text('2'), findsNothing);
 
@@ -23,7 +23,15 @@ void main() {
     await tester.tap(find.byIcon(Icons.add));
     await tester.pump();
 
-    // Verify that our counter has incremented.
+    // Verify that our counter is still 1.
+    expect(find.text('1'), findsOneWidget);
+    expect(find.text('2'), findsNothing);
+
+    // Tap the '+' icon and trigger a frame again.
+    await tester.tap(find.byIcon(Icons.add));
+    await tester.pump();
+
+    // Verify that our counter is now 2.
     expect(find.text('1'), findsNothing);
     expect(find.text('2'), findsOneWidget);
   });
